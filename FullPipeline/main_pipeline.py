aa54afc5baa9086680e61cf903dd75a84e333a35
Maxime Girard <maxime.girard@telecom-paris.fr>
2025-01-16 11:22:53 +0100

    Further improvements to the pipeline

--- FullPipeline/main_pipeline.py
+++ FullPipeline/main_pipeline.py
@@ -11,25 +11,32 @@ from PIL import Image
 import json
 from config import *
 import torch
-
+import re
+import random
 
 class AugmentationPipeline:
     def __init__(
         self,
         dataset_path: str,
-        image_dir: str,
+        augmented_dir: str,
         lora_path: str,
         lora_name: str,
         focus_net_path: str,
+        excluded_categories: list = [],
+        replaced_categories: dict = {},
     ):
+        self.augmented_dir = augmented_dir
+        self.excluded_categories = excluded_categories
+        self.replaced_categories = replaced_categories
+        
         self.dataset = TrashCanDataset(os.path.join(dataset_path, "instances_train_trashcan.json"), os.path.join(dataset_path,"train"))
         self.inpainting = InpaintingPipeline()
-        self.inpainting.load_lora_weights(LORA_PATH, LORA_WEIGHTS)
+        self.inpainting.load_lora_weights(lora_path, lora_name)
         self.focus_net = FocusNetv4()
         self.focus_net.load_state_dict(torch.load(focus_net_path, weights_only=True))
         self.focus_net.eval()
         
-        self.prompts = prompts
+        self.prompts = prompts_v2
 
     def generate_one_augmented_image(self):
         """
@@ -41,11 +48,16 @@ class AugmentationPipeline:
         image_id = self.dataset.get_random_image_id()
         bbox, category = self.dataset.get_random_bbox_and_category(image_id)
         image = self.dataset.load_image(image_id)
-        while not is_image_convenient(image, bbox, 50, 50):
+        while not is_image_convenient(image, bbox, 50, 50) or category in self.excluded_categories:
             image_id = self.dataset.get_random_image_id()
             bbox, category = self.dataset.get_random_bbox_and_category(image_id)
             image = self.dataset.load_image(image_id)
-
+            
+        if category in self.replaced_categories:
+            category = random.choice(self.replaced_categories[category])
+        
+        print(f"Image ID: {image_id}, Category: {category}")
+        
         mask = generate_mask(image, bbox)
 
         #Apply inpainting with the model
@@ -54,7 +66,7 @@ class AugmentationPipeline:
             negative_prompt="text, clean, clear, nothing",
             init_image=image,
             mask_image=mask,
-            strength=1.0,
+            strength=0.975,
             padding_mask_crop=64,
             num_inference_steps=50,
         )
@@ -70,25 +82,48 @@ class AugmentationPipeline:
             "category": category,
         }
 
-        return image, inpainted_image, mask, metadata
+        return image, inpainted_image, metadata
 
-    def generate_augmented_images(self, output_dir: str, num_images: int = 10_000):
+    def generate_augmented_images(self, num_images: int = 10000, augmented_dir: str = None):
+        output_dir = augmented_dir if augmented_dir is not None else self.augmented_dir
         os.makedirs(output_dir, exist_ok=True)
 
         for i in tqdm(range(num_images), desc="Generating images"):
             try:
-                image, inpainted_image, mask, metadata = self.generate_one_augmented_image()
+                original_image, inpaited_image, metadata = self.generate_one_augmented_image()
             except ValueError as e:
                 print(f"Error: {e}")
                 continue
             # Save the image and metadata
-            impainted_image = draw_bbox(inpainted_image, metadata["original_bbox"])
-            impainted_image = draw_bbox(impainted_image, metadata["refined_bbox"], color='yellow')
-            impainted_image = draw_label(impainted_image, category_labels[metadata["category"]])
-            impainted_image.save(os.path.join(output_dir, f"image_{i}.png"))
-            #masked_image.save(os.path.join(output_dir, f"masked_image_{i}.png"))
+            inpaited_image.save(os.path.join(output_dir, f"image_{i}_{metadata['image_id']}.png"))
+            # add the fileanme to metadata
+            metadata["filename"] = f"image_{i}_{metadata['image_id']}.png"
             with open(os.path.join(output_dir, f"metadata_{i}.json"), "w") as f:
                 json.dump(metadata, f)
+                
+            # for the report
+            inpaited_image = draw_bbox(inpaited_image, metadata["original_bbox"])
+            inpaited_image = draw_bbox(inpaited_image, metadata["refined_bbox"], color='yellow')
+            inpaited_image = draw_label(inpaited_image, category_labels[metadata["category"]])
+            inpaited_image.save(os.path.join("for_the_report", f"image_{i}.png"))
+            original_image = draw_bbox(original_image, metadata["original_bbox"])
+            original_image.save(os.path.join("for_the_report", f"image_{i}_original.png"))            
+        
+        # merge all the json into one file
+        metadata = []
+        for filename in os.listdir(output_dir):
+        # Check if the filename matches the pattern "metadata_*.json"
+            if re.match(r"metadata_\d+\.json", filename):
+                file_path = os.path.join(output_dir, filename)
+                with open(file_path, "r") as f:
+                    metadata.append(json.load(f))
+
+        # Write the merged metadata to a single JSON file
+        output_file = os.path.join(output_dir, "augmented_trashcan.json")
+        with open(output_file, "w") as f:
+            json.dump(metadata, f, indent=4)
+
+        print(f"Merged metadata written to {output_file}")
 
 
 if __name__ == "__main__":
@@ -99,21 +134,8 @@ if __name__ == "__main__":
         LORA_PATH,
         LORA_WEIGHTS,
         FOCUS_NET_PATH,
+        excluded_categories=[1], # The rover is not relevant for the augmentation
+        replaced_categories={17: [9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22]} # Replace the unknown category with the trash categories
     )
     
-    # image, inpainted_image, mask, metadata = pipeline.generate_one_augmented_image()
-    
-    # bbox_image = draw_bbox(image, metadata["original_bbox"])
-    # bbox_inpaint = draw_bbox(inpainted_image, metadata["original_bbox"])
-    # bbox_inpaint = draw_bbox(bbox_inpaint, metadata["refined_bbox"], color='yellow')
-    
-    # # Save the image and metadata
-    # image.save("output_image.png")
-    # inpainted_image.save("output_inpainted_image.png")
-    # bbox_inpaint.save("output_bbox_inpaint.png")
-    # bbox_image.save("output_bbox_image.png")
-    # mask.save("output_mask.png")
-    
-    # print(metadata)
-
-    pipeline.generate_augmented_images("test_augmentation", 20)
+    pipeline.generate_augmented_images(5)
